836bb712d205af0c318e95adb6ce8d69db8dfe43
Emanuel Peña Aguilar <p.a.emanuel@gmail.com>
2016-05-06 19:55:50 -0500

    Initial Commit

---
+++ Calculadora/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Emanuel Peña Aguilar on 5/6/16.
+//  Copyright © 2016 Mezcal Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
