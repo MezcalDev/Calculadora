0135535a89430d82be9976189a80fdc0be9dfe0b
Emanuel Peña Aguilar <leg10n@Arwen.local>
2016-05-07 13:00:47 -0500

    single commit with the lesson content

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -3,13 +3,32 @@
 //  Calculadora
 //
 //  Created by Emanuel Peña Aguilar on 5/6/16.
+//  443 1685024 (Whatsapp)
+//  Skype: leg10n_
+//  emanuel@mezcaldev.com
 //  Copyright © 2016 Mezcal Development. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    enum Operator : String {
+        case Sum = "+"
+        case Substraction = "-"
+        case Multiplication = "*"
+        case Division = "/"
+    }
+
+    var currentOperator : Operator?
+    
+    
+    @IBOutlet weak var leftOperandTextField: UITextField!
+    
+    @IBOutlet weak var rightOperandTextField: UITextField!
+    
+    @IBOutlet weak var resultTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +39,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func numberAction(sender: UIButton) {
+        var number = (sender.titleLabel?.text)!
+        if let currentOperator = currentOperator {
+            rightOperandTextField.text = rightOperandTextField.text! + "\(number)"
+        } else {
+            leftOperandTextField.text = leftOperandTextField.text! + "\(number)"
+        }
+    }
+    
+    @IBAction func operatorAction(sender: UIButton) {
+        let selectedOperator = Operator(rawValue: (sender.titleLabel?.text)!)
+        currentOperator = selectedOperator
+    }
 
+    @IBAction func resultAction(sender: UIButton) {
+        if let currentOperator = currentOperator {
+            let leftHand = Double(leftOperandTextField.text!)!
+            let rightHand = Double(rightOperandTextField.text!)!
+            var result : Double = 0.0
+            switch currentOperator {
+            case .Sum:
+                result = leftHand + rightHand
+            case .Division:
+                result = leftHand / rightHand
+            case .Multiplication:
+                result = leftHand * rightHand
+            case .Substraction:
+                result = leftHand - rightHand
+            }
+            
+            resultTextField.text = "\(result)"
+        }
+    }
+    
 }
 
